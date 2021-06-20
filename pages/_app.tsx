318a98daaf1b6ad2d65d93bf25bba6835b903a72
Niko o <kolyabokov41@gmail.com>
2021-06-20 21:09:40 +0300

    recactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { MenuProvider } from '../context/contextMenu'
 
 import { animation } from '../animation/animation'
 import '../public/styles/globals.css'
+// @ts-ignore
 import Header from '../components/Header'
 
 function MyApp({ Component, pageProps, router }: AppProps) {
