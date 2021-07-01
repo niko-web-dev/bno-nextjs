1986526400974f30738518d9feed478070edcb69
Taras Senko <t.senko@viseven.com>
2021-07-01 03:17:46 +0300

    develop card

--- pages/Card.tsx
+++ pages/Card.tsx
@@ -1,7 +1,28 @@
 import { FC } from 'react'
+import CardLeft from '../components/cardLeft'
+import CardForm from '../components/cardForm'
 
-const Card: FC = () => {
-	return <>card</>
+
+const Card: FC<{}> = ({ products }) => {
+	return (
+		<>
+			<main className="card">
+				<div className="container">
+					<div className="card__wrapper">
+						<CardLeft products={products}> </CardLeft>
+						<CardForm />
+					</div>
+				</div>
+			</main>
+		</>
+	)
+}
+
+export async function getServerSideProps() {
+	const res = await fetch(`http://wp.iqwik.ru/wp-json/wp/v2/products/`)
+	const products = await res.json()
+
+	return { props: { products } }
 }
 
 export default Card
